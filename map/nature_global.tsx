c004e0ebf3bec56ec4adf72034c00bdb19ea4517
Gotvna <92024828+Gotvna@users.noreply.github.com>
2022-11-26 16:35:01 +0100

    add v1 transition map

---
+++ map/nature_global.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="global" tilewidth="16" tileheight="16" tilecount="130" columns="10">
+ <image source="../assets/nature/global.png" trans="ffffff" width="160" height="208"/>
+</tileset>
